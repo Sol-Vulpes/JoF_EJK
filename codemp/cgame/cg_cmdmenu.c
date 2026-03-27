7d5a7f4bd6a7e84ea889ce0ec7339462ffb5200d
Sol Vulpes <solshiretsuna@gmail.com>
2026-03-27 22:49:43 +0100

    Working sort of

--- codemp/cgame/cg_cmdmenu.c
+++ codemp/cgame/cg_cmdmenu.c
@@ -22,6 +22,7 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 #include "cg_local.h"
 #include "ui/keycodes.h"
+#include "ui/menudef.h"
 
 // ──────────────────────────────────────────────────────────────────────────────
 // Entry table
