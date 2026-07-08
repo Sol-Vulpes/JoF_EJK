226b6e4c6c7914cc40f900479dce517eca772384
LukasMoudry <lukas.moudry9973@gmail-com>
2026-07-08 13:24:36 +0200

    Plugin 10 logic inverted, now it says enabled when you can ledge grab

--- codemp/cgame/cg_xcvar.h
+++ codemp/cgame/cg_xcvar.h
@@ -187,7 +187,7 @@ XCVAR_DEF( cg_drawHud,							"1",		NULL,				CVAR_ARCHIVE )
 
 XCVAR_DEF( cg_predictKnockback,					"0",		NULL,				0 )
 
-XCVAR_DEF( cp_pluginDisable,					"1536",		NULL,				CVAR_ARCHIVE|CVAR_USERINFO ) //'enable' ledge grab (1536)
+XCVAR_DEF( cp_pluginDisable,					"512",		NULL,				CVAR_ARCHIVE|CVAR_USERINFO ) //bits mean disable: 512 = holstered saber off, ledge grab on
 XCVAR_DEF( com_maxFPS,							"125",		NULL,				CVAR_ARCHIVE )
 XCVAR_DEF( cg_displayCameraPosition,		"1 80 16",		NULL,				CVAR_ROM|CVAR_USERINFO )
 XCVAR_DEF( cg_displayNetSettings,			"125 0 125",	NULL,				CVAR_ROM|CVAR_USERINFO )
