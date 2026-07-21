494f001bb8e61572f883c3197b2ce8c00197cc2a
Ahwii9 <305846188+Ahwii9@users.noreply.github.com>
2026-07-21 17:02:24 +0100

    implemented cleanup for .tmp pk3 files.
    Removes any files left behind after either esc, crash or server disconnect.

--- codemp/client/cl_main.cpp
+++ codemp/client/cl_main.cpp
@@ -969,6 +969,22 @@ void CL_Disconnect( qboolean showMainMenu ) {
 		FS_FCloseFile( clc.download );
 		clc.download = 0;
 	}
+
+	// Remove any incomplete download .tmp file left behind.
+	if (*clc.downloadTempName) {
+		char* ospath;
+		//maybe hacky but engine requires os filesystem paths for fs_remove
+		ospath = FS_BuildOSPath(
+			Cvar_VariableString("fs_homepath"),
+			clc.downloadTempName,
+			""
+		);
+
+		ospath[strlen(ospath) - 1] = '\0';
+		Com_Printf("Removing: %s\n", ospath); //just for debug purposes, delete if neccesary.
+		FS_Remove(ospath);
+	}
+
 	*clc.downloadTempName = *clc.downloadName = 0;
 	Cvar_Set( "cl_downloadName", "" );
 
