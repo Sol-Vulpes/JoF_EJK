d1abfb6cddcbff3c4d1be38d7f6ca510711e9f01
lumaya <89487897+lumayaa@users.noreply.github.com>
2026-09-20 11:46:30 +0200

    Use generic icon for missing party portraits

--- codemp/cgame/cg_local.h
+++ codemp/cgame/cg_local.h
@@ -1503,6 +1503,7 @@ typedef struct cgMedia_s {
 	qhandle_t	charsetShader;
 	qhandle_t	whiteShader;
 	qhandle_t	binocularHudFont;
+	qhandle_t	missionPartyUnknownIcon;
 
 	qhandle_t	loadBarLED;
 	qhandle_t	loadBarLEDCap;
